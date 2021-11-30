ce7d9271f1d9470282b8ccbb8f019e448fe8d386
RuyaKang1997 <kangruya@gmail.com>
2021-11-29 23:48:45 -0500

    unet model

--- code/UNet.py
+++ code/UNet.py
@@ -19,21 +19,15 @@ class Model(tf.keras.Model):
         self.optimizer = Adam(1e-5)
         self.loss_list = []
 
-        # self.encoder1 = self.block(16)
-        # self.encoder2 = self.block(32, max_pool=True)
-        #
-        # self.decoder1 = self.block(64, max_pool=True, up_sample=True)
-        # self.decoder2 = self.block(32, up_sample=True)
+        self.encoder1 = self.block(32)
+        self.encoder2 = self.block(64, max_pool=True)
+        self.encoder3 = self.block(128, max_pool=True)
 
-        self.encoder1 = self.block(4)
-        self.encoder2 = self.block(8, max_pool=True)
-        self.encoder3 = self.block(16, max_pool=True)
+        self.decoder1 = self.block(256, max_pool=True, up_sample=True)
+        self.decoder2 = self.block(128, up_sample=True)
+        self.decoder3 = self.block(64, up_sample=True)
 
-        self.decoder1 = self.block(32, max_pool=True, up_sample=True)
-        self.decoder2 = self.block(16, up_sample=True)
-        self.decoder3 = self.block(8, up_sample=True)
-
-        self.output_prob = self.block(4, output=True)
+        self.output_prob = self.block(32, output=True)
 
     def block(self, filters, max_pool=False, up_sample=False, output=False):
 
@@ -61,17 +55,6 @@ class Model(tf.keras.Model):
         :param inputs: images, shape of (num_inputs, 256, 256, 1); during training, the shape is (batch_size, 256, 256, 1)
         :return: probs - a matrix of shape (num_inputs, 256, 256, 3); during training, it would be (batch_size, 256, 256, 3)
         """
-
-        # encode1 = self.encoder1(inputs)
-        # encode2 = self.encoder2(encode1)
-        #
-        # decode1 = self.decoder1(encode2)
-        # merge1 = concatenate([encode2, decode1], 3)
-        #
-        # decode2 = self.decoder2(merge1)
-        # merge2 = concatenate([encode1, decode2], 3)
-        #
-        # out = self.output_prob(merge2)
 
         encode1 = self.encoder1(inputs)
         encode2 = self.encoder2(encode1)
@@ -172,7 +155,7 @@ def test(model, test_inputs, test_labels):
 
 def main():
 
-    train_inputs, train_labels = get_data('../data/train_img_r.npy', '../data/train_lab_r.npy', 'flip')
+    train_inputs, train_labels = get_data('../data/train_img_r.npy', '../data/train_lab_r.npy', 'both')
     train_inputs = tf.reshape(train_inputs, (train_inputs.shape[0], 256, 256, 1))
     test_inputs, test_labels = get_data('../data/test_img_r.npy', '../data/test_lab_r.npy')
     test_inputs = tf.reshape(test_inputs, (test_inputs.shape[0], 256, 256, 1))
@@ -183,12 +166,12 @@ def main():
     # train
     for i in range(50):
         train(model, train_inputs, train_labels)
-        print(f"Train Epoch: {i} \tLoss: {np.mean(model.loss_list):.6f}")
+        print(f"Train Epoch: {i}  Loss: {np.mean(model.loss_list):.6f}")
         iou, acc, mean_acc = test(model, test_inputs, test_labels)
         print(f"--IoU: {iou:.6f}  --pixel accuracy: {acc:.6f}  --mean pixel accuracy: {mean_acc:.6f}")
 
         if (i + 1) % 10 == 0:
-            show_seg(model, test_inputs[:10], test_labels[:10], 'unet_flip' + str(i + 1))
+            show_seg(model, test_inputs[:10], test_labels[:10], 'unet_both' + str(i + 1))
 
 
 if __name__ == '__main__':
